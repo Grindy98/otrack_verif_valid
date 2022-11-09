ca1c5c98eda63e50c0cb7480c7aba260433b2c72
Stefan Malanik <stefanmalanik@gmail.com>
2022-11-09 22:07:09 +0100

    Added pickle utils for persistent storage

---
+++ source/persistent.py
@@ -0,0 +1,17 @@
+import pickle
+_state_dict = None
+
+def load_from_save(filename):
+    # TODO: check if filename exists, else empty state dict
+    with open(filename, 'r') as infile:
+        _state_dict = pickle.load(infile)
+
+def dump_to_save(filename):
+    with open(filename, 'r') as outfile:
+        pickle.dump(_state_dict, outfile)
+
+def get_class_list(cls : type):
+    # TODO: state dict structure is a dict of lists of classes having the same type
+    if cls not in _state_dict:
+        _state_dict[cls] = []
+    return _state_dict[cls]
