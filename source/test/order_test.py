7d3a31e16b773bd11e863a8ff7a04f08589594fb
Ant <eu.antonioc@gmail.com>
2022-11-21 16:50:12 +0100

    Fixed tests with strings

--- source/test/order_test.py
+++ source/test/order_test.py
@@ -8,23 +8,23 @@ from source.test.product_test import pers_product_create_mult
 
 
 def test_B23(run, pers_product_create_mult, pers_client_create_mult):
-    assert run(['orders_add', 1, 'bnn01', 3, '21/11/1999']) == \
+    assert run(['orders_add', '1', 'bnn01', '3', '21/11/1999']) == \
         ''
         
 def test_A18(run, pers_product_create_mult, pers_client_create_mult):
-    assert run(['orders_add', 100, 'ctmt01', 3, '21/11/1999']) == \
+    assert run(['orders_add', '100', 'ctmt01', '3', '21/11/1999']) == \
         "No client matches with this information."
         
 def test_A19(run, pers_product_create_mult, pers_client_create_mult):
-    assert run(['orders_add', 1, 'axolotl', 70, '21/11/1999']) == \
+    assert run(['orders_add', '1', 'axolotl', '70', '21/11/1999']) == \
         "No product matches with this information."
         
 def test_A20(run, pers_product_create_mult, pers_client_create_mult):
-    assert run(['orders_add', 1, 'ctmt01', -1, '21/11/1999']) == \
+    assert run(['orders_add', '1', 'ctmt01', '-1', '21/11/1999']) == \
         "Invalid data format."
         
 def test_A21(run, pers_product_create_mult, pers_client_create_mult):
-    assert run(['orders_add', 1, 'ctmt01', 3, '21-11-1999']) == \
+    assert run(['orders_add', '1', 'ctmt01', '3', '21-11-1999']) == \
         "Invalid data format."
         
 def test_A22(run, pers_order_create):
