893fd124c842ee8b89b42252eaa5a85e79d5516b
Stefan Malanik <stefanmalanik@gmail.com>
2022-12-16 18:46:54 +0100

    Fix of persistent state

--- source/persistent.py
+++ source/persistent.py
@@ -4,6 +4,7 @@ import os
 _state_dict = None
 
 def load_from_save(filename):
+    global _state_dict
     try:
         with open(filename, 'rb') as infile:
             _state_dict = pickle.load(infile)
@@ -13,10 +14,12 @@ def load_from_save(filename):
         _state_dict = {}
 
 def dump_to_save(filename):
+    global _state_dict
     with open(filename, 'wb') as outfile:
         pickle.dump(_state_dict, outfile)
 
 def get_class_list(cls : type):
+    global _state_dict
     # TODO: state dict structure is a dict of lists of classes having the same type
     if cls not in _state_dict:
         _state_dict[cls] = []
